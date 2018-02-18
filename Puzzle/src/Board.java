f86e45e99626089d422ca07b82f94c42e3c1d120
Ievgen Melnyk <nehvey@Ievgens-MBP.local>
2018-02-19 00:39:47 +0100

    8puzzle intitial commit

---
+++ Puzzle/src/Board.java
@@ -0,0 +1,106 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+
+public class Board {
+
+    private final int[][] blocks;
+    private final int n;
+
+    // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    public Board(int[][] blocks) {
+        this.blocks = blocks;
+        n = blocks.length;
+    }
+
+    // board dimension n
+    public int dimension() {
+        return 0;
+    }
+
+    // number of blocks out of place
+    public int hamming() {
+        return 0;
+    }
+
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+        return 0;
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        return false;
+    }
+
+    // a board that is obtained by exchanging any pair of blocks
+    public Board twin() {
+        return null;
+    }
+
+    // does this board equal other?
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (other == null) return false;
+        if (other.getClass() != this.getClass()) return false;
+        Board that = (Board) other;
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < n; k++) {
+                if (this.blocks[i][k] != that.blocks[i][k]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+        return new Iterable<Board>() {
+            @Override
+            public Iterator<Board> iterator() {
+                return null;
+            }
+        };
+    }
+
+    // string representation of this board (in the output format specified below)
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(n + "\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                s.append(String.format("%2d ", blocks[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    // unit tests (not graded)
+    public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }
+}
