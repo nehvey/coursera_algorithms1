b6f0c94c6d73f6e68cc6dc25feef6d2ca1389056
imelnyk <imelnyk@360t.com>
2018-02-20 18:36:19 +0100

    solver + neighbors

--- Puzzle/src/Board.java
+++ Puzzle/src/Board.java
@@ -1,7 +1,11 @@
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 public class Board {
 
@@ -15,13 +19,14 @@ public class Board {
 	// (where blocks[i][j] = block in row i, column j)
 	public Board(int[][] blocks) {
 		n = blocks.length;
-		this.blocks = blocks;
+		this.blocks = new int[n][n];
 		goal = new int[n][n];
 		xCoordinates = new int[n * n];
 		yCoordinates = new int[n * n];
 		int j = 0;
 		for (int i = 0; i < n; i++) {
 			for (int k = 0; k < n; k++) {
+				this.blocks[i][k] = blocks[i][k];
 				goal[i][k] = j;
 				xCoordinates[j] = i;
 				yCoordinates[j] = k;
@@ -105,17 +110,47 @@ public class Board {
 
 	// all neighboring boards
 	public Iterable<Board> neighbors() {
-		return () -> new Iterator<Board>() {
-			@Override
-			public boolean hasNext() {
-				return false;
+		List<Board> neighbors = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			for (int k = 0; k < n; k++) {
+				final int block = blocks[i][k];
+				if (block == 0) {
+					// up
+					if (i - 1 >= 0) {
+						int[][] blocksCopy = Arrays.stream(blocks).map(int[]::clone).toArray(int[][]::new);
+						final int tmp = blocksCopy[i - 1][k];
+						blocksCopy[i - 1][k] = block;
+						blocksCopy[i][k] = tmp;
+						neighbors.add(new Board(blocksCopy));
+					}
+					// down
+					if (i + 1 < n) {
+						int[][] blocksCopy = Arrays.stream(blocks).map(int[]::clone).toArray(int[][]::new);
+						final int tmp = blocksCopy[i + 1][k];
+						blocksCopy[i + 1][k] = block;
+						blocksCopy[i][k] = tmp;
+						neighbors.add(new Board(blocksCopy));
+					}
+					// left
+					if (k - 1 >= 0) {
+						int[][] blocksCopy = Arrays.stream(blocks).map(int[]::clone).toArray(int[][]::new);
+						final int tmp = blocksCopy[i][k - 1];
+						blocksCopy[i][k - 1] = block;
+						blocksCopy[i][k] = tmp;
+						neighbors.add(new Board(blocksCopy));
+					}
+					// right
+					if (k + 1 < n) {
+						int[][] blocksCopy = Arrays.stream(blocks).map(int[]::clone).toArray(int[][]::new);
+						final int tmp = blocksCopy[i][k + 1];
+						blocksCopy[i][k + 1] = block;
+						blocksCopy[i][k] = tmp;
+						neighbors.add(new Board(blocksCopy));
+					}
+				}
 			}
-
-			@Override
-			public Board next() {
-				return null;
-			}
-		};
+		}
+		return neighbors;
 	}
 
 	// string representation of this board (in the output format specified below)
@@ -146,6 +181,12 @@ public class Board {
 		System.out.println(initial.toString());
 		System.out.println("Hamming - " + initial.hamming());
 		System.out.println("Manhattan - " + initial.manhattan());
+
+		System.out.println("~~~~~~~~~~~Neighbors~~~~~~~~~~");
+		for (Board board : initial.neighbors()) {
+			System.out.println(board);
+		}
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
 		// solve the puzzle
 		Solver solver = new Solver(initial);
--- Puzzle/src/Solver.java
+++ Puzzle/src/Solver.java
@@ -1,9 +1,26 @@
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Comparator;
 import java.util.Iterator;
 
 public class Solver {
+
+	private BoardComparator boardComparator = new BoardComparator();
+	private MinPQ<Board> pq = new MinPQ<>(boardComparator);
+	private int moves = 0;
+
 	// find a solution to the initial board (using the A* algorithm)
 	public Solver(Board initial) {
-
+		// insert the initial search node (the initial board, 0 moves, and a null predecessor search node) into a priority queue
+		pq.insert(initial);
+		Board min;
+		while (!(min = pq.delMin()).isGoal()) {
+//			System.out.println(min);
+			for (Board neighbor : min.neighbors()) {
+				pq.insert(neighbor);
+			}
+			moves++;
+		}
 	}
 
 	// is the initial board solvable?
@@ -13,7 +30,7 @@ public class Solver {
 
 	// min number of moves to solve initial board; -1 if unsolvable
 	public int moves() {
-		return 0;
+		return moves;
 	}
 
 	// sequence of boards in a shortest solution; null if unsolvable
@@ -29,6 +46,13 @@ public class Solver {
 				return null;
 			}
 		};
+	}
+
+	private class BoardComparator implements Comparator<Board> {
+		@Override
+		public int compare(Board b1, Board b2) {
+			return b1.manhattan() - b2.manhattan();
+		}
 	}
 
 	// solve a slider puzzle (given below)
