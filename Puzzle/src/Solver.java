dec58c4b25c84e5df89d9572768df7a74c499f71
imelnyk <imelnyk@360t.com>
2018-02-27 18:23:45 +0100

    check if solvable

--- Puzzle/src/Solver.java
+++ Puzzle/src/Solver.java
@@ -6,86 +6,102 @@ import java.util.LinkedList;
 
 public class Solver {
 
-    private static final BoardComparator BOARD_COMPARATOR = new BoardComparator();
-    private final MinPQ<SearchNode> pq = new MinPQ<>(BOARD_COMPARATOR);
-    // private Set<Board> predecessors = new HashSet<>();
-    private final Deque<Board> solutionBoards = new LinkedList<>();
+	private static final BoardComparator BOARD_COMPARATOR = new BoardComparator();
+	private final MinPQ<SearchNode> pqMain = new MinPQ<>(BOARD_COMPARATOR);
+	private final MinPQ<SearchNode> pqTwin = new MinPQ<>(BOARD_COMPARATOR);
+	private final Deque<Board> solutionBoards = new LinkedList<>();
+	private boolean solvable = true;
 
-    // find a solution to the initial board (using the A* algorithm)
-    public Solver(Board initial) {
-        // insert the initial search node (the initial board, 0 moves, and a null predecessor search node) into a priority queue
-        pq.insert(new SearchNode(initial, null, 0));
-        SearchNode min = null;
-        while (!pq.isEmpty() && !(min = pq.delMin()).board.isGoal()) {
-            // if (predecessors.contains(min.board)) {
-            // continue;
-            // }
-            // System.out.println(min.board);
-            for (Board neighbor : min.board.neighbors()) {
-                final SearchNode searchNodePredecessor = new SearchNode(min.board, min.predecessor, min.moves);
-                if (searchNodePredecessor.board.equals(neighbor)) {
-                    continue;
-                }
-                pq.insert(new SearchNode(neighbor, searchNodePredecessor, min.moves + 1));
-            }
+	// find a solution to the initial board (using the A* algorithm)
+	public Solver(Board initial) {
+		// insert the initial search node (the initial board, 0 moves, and a null predecessor search node) into a priority queue
+		pqMain.insert(new SearchNode(initial, null, 0));
+		pqTwin.insert(new SearchNode(initial.twin(), null, 0));
+		SearchNode minMain = null;
+		while (!pqMain.isEmpty() && !(minMain = pqMain.delMin()).board.isGoal()) {
+			SearchNode minTwin = pqTwin.delMin();
+			if (minTwin.board.isGoal()) {
+				solvable = false;
+				return;
+			}
 
-            // predecessors.add(min.board);
-        }
+			// process twin
+			for (Board neighbor : minTwin.board.neighbors()) {
+				final SearchNode searchNodePredecessor = new SearchNode(minTwin.board, minTwin.predecessor, minTwin.moves);
+				if (searchNodePredecessor.board.equals(neighbor)) {
+					continue;
+				}
+				pqTwin.insert(new SearchNode(neighbor, searchNodePredecessor, minTwin.moves + 1));
+			}
 
-        assert min != null;
+			// process main
+			for (Board neighbor : minMain.board.neighbors()) {
+				final SearchNode searchNodePredecessor = new SearchNode(minMain.board, minMain.predecessor, minMain.moves);
+				if (searchNodePredecessor.board.equals(neighbor)) {
+					continue;
+				}
+				pqMain.insert(new SearchNode(neighbor, searchNodePredecessor, minMain.moves + 1));
+			}
 
-        SearchNode predecessor = min.predecessor;
-        if (predecessor != null) {
-            do {
-                solutionBoards.addFirst(predecessor.board);
-            } while ((predecessor = predecessor.predecessor) != null);
-        }
-    }
+			// predecessors.add(minMain.board);
+		}
 
-    // is the initial board solvable?
-    public boolean isSolvable() {
-        return true;
-    }
+		assert minMain != null;
 
-    // min number of moves to solve initial board; -1 if unsolvable
-    public int moves() {
-        if (isSolvable()) {
-            return solutionBoards.size();
-        }
-        return -1;
-    }
+		solutionBoards.addFirst(minMain.board);
 
-    // sequence of boards in a shortest solution; null if unsolvable
-    public Iterable<Board> solution() {
-        if (isSolvable()) {
-            return solutionBoards;
-        }
-        return null;
-    }
+		SearchNode predecessor = minMain.predecessor;
+		if (predecessor != null) {
+			do {
+				solutionBoards.addFirst(predecessor.board);
+			} while ((predecessor = predecessor.predecessor) != null);
+		}
+	}
 
-    private static class BoardComparator implements Comparator<SearchNode> {
-        @Override
-        public int compare(SearchNode b1, SearchNode b2) {
-            // return (b1.board.hamming() + b1.moves) - (b2.board.hamming() + b2.moves);
-            return (b1.board.manhattan() + b1.moves) - (b2.board.manhattan() + b2.moves);
-        }
-    }
+	// is the initial board solvable?
+	public boolean isSolvable() {
+		return solvable;
+	}
 
-    private class SearchNode {
-        Board board;
-        SearchNode predecessor;
-        int moves;
+	// min number of moves to solve initial board; -1 if unsolvable
+	public int moves() {
+		if (isSolvable()) {
+			return solutionBoards.size();
+		}
+		return -1;
+	}
 
-        SearchNode(Board board, SearchNode predecessor, int moves) {
-            this.board = board;
-            this.predecessor = predecessor;
-            this.moves = moves;
-        }
-    }
+	// sequence of boards in a shortest solution; null if unsolvable
+	public Iterable<Board> solution() {
+		if (isSolvable()) {
+			return solutionBoards;
+		}
+		return null;
+	}
 
-    // solve a slider puzzle (given below)
-    public static void main(String[] args) {
-        //TODO
+	private static class BoardComparator implements Comparator<SearchNode> {
+		@Override
+		public int compare(SearchNode b1, SearchNode b2) {
+			// return (b1.board.hamming() + b1.moves) - (b2.board.hamming() + b2.moves);
+			return (b1.board.manhattan() + b1.moves) - (b2.board.manhattan() + b2.moves);
+		}
+	}
 
-    }
+	private class SearchNode {
+		Board board;
+		SearchNode predecessor;
+		int moves;
+
+		SearchNode(Board board, SearchNode predecessor, int moves) {
+			this.board = board;
+			this.predecessor = predecessor;
+			this.moves = moves;
+		}
+	}
+
+	// solve a slider puzzle (given below)
+	public static void main(String[] args) {
+		// TODO
+
+	}
 }
