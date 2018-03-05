0d730b61b6a863288ac0bde712bbd470ecc05024
imelnyk <imelnyk@360t.com>
2018-03-05 14:56:45 +0100

    brute force

--- Kdtree/src/KdTree.java
+++ Kdtree/src/KdTree.java
@@ -1,7 +1,15 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
+import java.util.AbstractMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class KdTree {
+
+    private Set<Point2D> points = new HashSet<>();
+
     // construct an empty set of points
     public KdTree() {
 
@@ -19,7 +27,7 @@ public class KdTree {
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
-
+        points.add(p);
     }
 
     // does the set contain point p?
@@ -29,17 +37,18 @@ public class KdTree {
 
     // draw all points to standard draw
     public void draw() {
-
+        points.forEach(Point2D::draw);
     }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        return points.stream().filter(rect::contains).collect(Collectors.toSet());
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return null;
+        return points.stream().map(point -> new AbstractMap.SimpleEntry<>(point, p.distanceTo(point))).min(
+                (p1, p2) -> p1.getValue() > p2.getValue() ? 1 : -1).get().getKey();
     }
 
     // unit testing of the methods (optional)
--- Kdtree/src/PointSET.java
+++ Kdtree/src/PointSET.java
@@ -1,55 +1,56 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PointSET {
 
-	private Set<Point2D> points = new HashSet<>();
+    private Set<Point2D> points = new HashSet<>();
 
-	// construct an empty set of points
-	public PointSET() {
+    // construct an empty set of points
+    public PointSET() {
 
-	}
+    }
 
-	// is the set empty?
-	public boolean isEmpty() {
-		return false;
-	}
+    // is the set empty?
+    public boolean isEmpty() {
+        return false;
+    }
 
-	// number of points in the set
-	public int size() {
-		return -1;
-	}
+    // number of points in the set
+    public int size() {
+        return -1;
+    }
 
-	// add the point to the set (if it is not already in the set)
-	public void insert(Point2D p) {
-		points.add(p);
-	}
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        points.add(p);
+    }
 
-	// does the set contain point p?
-	public boolean contains(Point2D p) {
-		return false;
-	}
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        return false;
+    }
 
-	// draw all points to standard draw
-	public void draw() {
-		points.forEach(Point2D::draw);
-	}
+    // draw all points to standard draw
+    public void draw() {
+        points.forEach(Point2D::draw);
+    }
 
-	// all points that are inside the rectangle (or on the boundary)
-	public Iterable<Point2D> range(RectHV rect) {
-		return null;
-	}
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        return points.stream().filter(rect::contains).collect(Collectors.toSet());
+    }
 
-	// a nearest neighbor in the set to point p; null if the set is empty
-	public Point2D nearest(Point2D p) {
-		return null;
-	}
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        return points.stream().map(point -> new AbstractMap.SimpleEntry<>(point, p.distanceTo(point))).min(
+                (p1, p2) -> p1.getValue() > p2.getValue() ? 1 : -1).get().getKey();
+    }
 
-	// unit testing of the methods (optional)
-	public static void main(String[] args) {
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
 
-	}
+    }
 }
